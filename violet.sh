83774ed9af5763a3c3fcb1d97f953158d41942a4
chethazz <chethaskp10@gmail.com>
2022-04-07 18:36:16 +0530

    Switch to azure kernel

--- violet.sh
+++ violet.sh
@@ -5,7 +5,7 @@ echo "Cloning Vendor Trees"
 github_clone PixelOS-Devices/vendor_xiaomi_violet.git vendor/xiaomi/violet twelve
 
 echo "Cloning Kernel Trees"
-github_clone chethazz/android_kernel_xiaomi_violet.git kernel/xiaomi/violet twelve
+github_clone Panchajanya1999/msm-4.14.git kernel/xiaomi/violet R
 
 echo "Cloning Custom Hals"
 nuke hardware/qcom-caf/sm8150/audio
